9cc5feb0aed0848e24f6c8c1611dbc1e40e389fa
Rolex <101451384+r0l3x007@users.noreply.github.com>
2023-05-18 15:55:28 +0300

    Modified game logic, added user prompt

--- myscrip.js
+++ myscrip.js
@@ -5,29 +5,84 @@ function getComputerChoice(){
     return choices[randomChoice];
 }
 
+//calls gets user inputs and returs it in lower case for accurate comparasion
+function getUserInput(){
+let userInput = prompt("Choose your weapon!");
+if(userInput.toLowerCase() === "rock" || userInput.toLowerCase() === "paper" || userInput.toLowerCase() === "scissors"){
+    alert(`your choice is ${userInput}`);
+    return userInput.toLowerCase();
+} else {
+    alert("Wrong weapon!");
+}
+}
+
+//checking to make sure inputs arre correctly taken
+let playerSelection = getUserInput();
+let computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playerSelection);
+
 
 function playGame(playerSelection,computerSelection){
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
-
+    
     if(playerSelection === computerSelection){
-        return "Nobody wins its a tie!";
+        /*let tie = "Nobody wins its a tie!"
+        userScore++;
+        computerScore++
+        console.log(userScore);
+        console.log(computerScore);*/
+        return null ;
     }
     else if (playerSelection == 'rock' && computerSelection == 'scissors'){
-        return `Congratulations you win! ${playerSelection} beats ${computerSelection}`;
+        /*let winCase1 = `Congratulations you win! ${playerSelection} beats ${computerSelection}`;
+        userScore++;
+        console.log(userScore);*/
+        return true;
     }
     else if (playerSelection == 'scissors' && computerSelection == 'paper'){
-        return `Congratulations you win! ${playerSelection} beats ${computerSelection}` ;
+        /*let winCase2 = `Congratulations you win! ${playerSelection} beats ${computerSelection}`;
+        userScore++;
+        console.log(userScore);*/
+        return true;
     }
     else if (playerSelection == 'paper' && computerSelection == 'rock'){
-        return `Congratulations you win! ${playerSelection} beats ${computerSelection}`;
+        /*let winCase3 = `Congratulations you win! ${playerSelection} beats ${computerSelection}`;
+        userScore++;
+        console.log(userScore);*/
+        return true;
     } else {
-        return `Sorry you lose! ${computerSelection} beats ${playerSelection}`;
+        let lose = `Sorry you lose! ${computerSelection} beats ${playerSelection}`;
+        /*computerScore++;
+        console.log(computerScore);*/
+        return false;
     }
+    
+
+    
 }
 
-//testing the game
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-console.log(playGame(playerSelection,computerSelection));
+//there needs to be 5 rounds in total
+//there has to be 2 scores kept in check and if it is a tie then adding it to each score
+//at the beginning of each round a new input needs to be made
+function game(){
+   let userScore = 0;
+   let computerScore = 0;
+   if (playGame(playerSelection,computerSelection) ===  true){
+    userScore++;
+    console.log(userScore);
+    console.log(computerScore);
+    console.log(`Congratulations you win! ${playerSelection} beats ${computerSelection}`);
+   } else if (playGame(playerSelection,computerSelection) === false){
+    computerScore++;
+    console.log(computerScore);
+    console.log(userScore);
+    console.log(`Sorry you lose! ${computerSelection} beats ${playerSelection}`);
+   } else {
+    console.log(userScore);
+    console.log(computerScore);
+    console.log("Nobody wins its a tie!");
+   }
+    
+}
