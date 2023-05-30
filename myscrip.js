de531ca2e300a19b41fa7ced251a9965060e4db4
Rolex <101451384+r0l3x007@users.noreply.github.com>
2023-05-30 13:10:28 +0300

    Added dynamic score tracking and displaying the winner

--- myscrip.js
+++ myscrip.js
@@ -14,45 +14,6 @@ let computerScore = 0;
 //console.log(playerSelection);
 
 
-function playGame(playerSelection,computerSelection){
-    //playerSelection = prompt("Choose your weapon!",''.toLowerCase());
-    computerSelection = getComputerChoice().toLowerCase();
-    console.log(playerSelection);
-    console.log(computerSelection);
-
-    if(playerSelection === computerSelection){
-        alert("Nobody wins its a tie!");
-        console.log(`your score ${userScore}`);
-        console.log(`computer score ${computerScore}`);
-    }
-    else if (playerSelection == 'rock' && computerSelection == 'scissors'){
-        alert(`Congratulations you win! ${playerSelection} beats ${computerSelection}`);
-        userScore++;
-        console.log(`your score ${userScore}`);
-        console.log(`computer score ${computerScore}`);
-    }
-    else if (playerSelection == 'scissors' && computerSelection == 'paper'){
-        alert(`Congratulations you win! ${playerSelection} beats ${computerSelection}`);
-        userScore++;
-        console.log(`your score ${userScore}`);
-        console.log(`computer score ${computerScore}`);
-    }
-    else if (playerSelection == 'paper' && computerSelection == 'rock'){
-        alert(`Congratulations you win! ${playerSelection} beats ${computerSelection}`);
-        userScore++;
-        console.log(`your score ${userScore}`);
-        console.log(`computer score ${computerScore}`);
-    } else {
-        alert(`Sorry you lose! ${computerSelection} beats ${playerSelection}`)
-        computerScore++;
-        console.log(`your score ${userScore}`);
-        console.log(`computer score ${computerScore}`);
-    }
-        
-}
-//I have 3 buttons with 3 choices each
-//I need to assign the choice as a parameter in the playgame function
-
 document.getElementById(`rock`).addEventListener(`click`, function(){
     playGame(`rock`, computerSelection);
 });
@@ -64,6 +25,70 @@ document.getElementById(`scissors`).addEventListener(`click`, function(){
 document.getElementById(`paper`).addEventListener(`click`, function(){
     playGame(`paper`, computerSelection);
 });
+
+
+const scores =  document.querySelector(`#score`);
+//if(scores){
+//scores.textContent = `Your score is ${userScore} and the computers score is ${computerScore}`;
+//}
+const winners =  document.querySelector(`#winner`);
+const finalWin =  document.querySelector(`#finalWinner`);
+
+function playGame(playerSelection,computerSelection){
+    //playerSelection = prompt("Choose your weapon!",''.toLowerCase());
+    computerSelection = getComputerChoice().toLowerCase();
+    console.log(playerSelection);
+    console.log(computerSelection);
+
+    if(playerSelection === computerSelection){
+        /*alert("Nobody wins its a tie!");
+        console.log(`your score ${userScore}`);
+        console.log(`computer score ${computerScore}`);*/
+        winners.textContent = `Nobody wins its a tie`;
+        scores.textContent = `Your score is ${userScore} and the computers score is ${computerScore}`;
+    }
+    else if (playerSelection == 'rock' && computerSelection == 'scissors'){
+       // alert(`Congratulations you win! ${playerSelection} beats ${computerSelection}`);
+        userScore++;
+       // console.log(`your score ${userScore}`);
+        //console.log(`computer score ${computerScore}`);
+        winners.textContent = `Congratulations you win! ${playerSelection} beats ${computerSelection}`;
+        scores.textContent = `Your score is ${userScore} and the computers score is ${computerScore}`;
+    }
+    else if (playerSelection == 'scissors' && computerSelection == 'paper'){
+        //alert(`Congratulations you win! ${playerSelection} beats ${computerSelection}`);
+        userScore++;
+       // console.log(`your score ${userScore}`);
+        //console.log(`computer score ${computerScore}`);
+        winners.textContent = `Congratulations you win! ${playerSelection} beats ${computerSelection}`;
+        scores.textContent = `Your score is ${userScore} and the computers score is ${computerScore}`;
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'rock'){
+        //alert(`Congratulations you win! ${playerSelection} beats ${computerSelection}`);
+        userScore++;
+        //console.log(`your score ${userScore}`);
+       // console.log(`computer score ${computerScore}`);
+       winners.textContent = `Congratulations you win! ${playerSelection} beats ${computerSelection}`;
+       scores.textContent = `Your score is ${userScore} and the computers score is ${computerScore}`;
+    } else {
+       // alert(`Sorry you lose! ${computerSelection} beats ${playerSelection}`)
+        computerScore++;
+       // console.log(`your score ${userScore}`);
+       // console.log(`computer score ${computerScore}`);
+       winners.textContent = `Sorry you lose! ${computerSelection} beats ${playerSelection}`;
+       scores.textContent = `Your score is ${userScore} and the computers score is ${computerScore}`;
+    }
+
+
+    if (computerScore == 5 && computerScore > userScore){
+        finalWin.textContent = `Sorry  the computer reached 5 first, you lose`;
+    } else if  ( userScore == 5 &&  userScore > computerScore){
+        finalWin.textContent = `Congratulations you reached 5 first, you win yeeey.`;
+    }
+}
+
+
+
 
 //there needs to be 5 rounds in total
 //there has to be 2 scores kept in check and if it is a tie then adding it to each score
